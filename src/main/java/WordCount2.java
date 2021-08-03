599f1a0f8c3b49760a519c47f95574947aa37d36
Garima Gupta <62745460+garimagupta1507@users.noreply.github.com>
2021-08-03 14:12:15 -0700

    Added WordCount class (#3)
    * Added WordCount class
    
    * updated WordCount class and added case uppercaseWordcount (WordCount2) class
    
    * updated WordCount and  uppercaseWordcount (WordCount2) class
    
    Co-authored-by: Garima Gupta <garimagupta@Garimas-MacBook-Pro-2.local>

--- src/main/java/WordCount2.java
+++ src/main/java/WordCount2.java
@@ -2,10 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
-public class WordCount {
+public class WordCount2 {
     public static void main(String[] args){
         String line;
         Map<String, Integer> counter = new HashMap<String, Integer>();
@@ -13,8 +12,10 @@ public class WordCount {
             FileReader fileReader = new FileReader("src/main/resources/sample.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             while((line = bufferedReader.readLine()) != null) {
-                String[] words = line.split(" ");
+                String line2 = line.toUpperCase();
+                String[] words = line2.split(" ");
                 for(int i = 0; i < words.length; i++){
+                    //System.out.println(words[i]);
                     if(counter.containsKey(words[i])){
                         counter.put(words[i], counter.get(words[i]) + 1);
                     } else {
@@ -31,4 +32,4 @@ public class WordCount {
 }
 
 //OUTPUT
-//{Ashu=1, Masters=1, fathers=1, Hello,=1, brothers=1, in=1, Science=1, I=1, is=4, California=1, My=4, Sunita=1, my=1, Gupta=4, Garima=1, mothers=1, Dinesh=1, State=1, name=4, Computer=1, from=1, did=1, University.=1}
+//{MOTHERS=1, ASHU=1, DINESH=1, IN=1, GARIMA=1, UNIVERSITY.=1, I=1, STATE=1, IS=4, FROM=1, MY=5, NAME=4, BROTHERS=1, MASTERS=1, CALIFORNIA=1, COMPUTER=1, HELLO,=1, FATHERS=1, SCIENCE=1, SUNITA=1, DID=1, GUPTA=4}
