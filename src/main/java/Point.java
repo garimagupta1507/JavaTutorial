e4b77925d941f506ef01661c2bbc4222562a40dd
Garima Gupta <garimagupta@Garimas-MacBook-Pro-2.local>
2021-08-05 15:46:55 -0700

    added Rectangle class

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,34 +1,13 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Point {
-    private int x;
-    private int y;
+    double x;
+    double y;
 
-    public void main(String[] input) {
-        int count = 0;
-        String line;
-        try {
-            FileReader fileReader = new FileReader("src/main/resources/point.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while((line = bufferedReader.readLine()) != null) {
-                String[] nums = line.split(",");
-                Integer[][] intNum = new Integer[2][2];
-                for(int i = 0; i < nums.length; i++) {
-                    for (int j = i + 1; j < nums.length - 1; j++){
-                        intNum[i][j] = Integer.parseInt(nums[i],[j]);
-                    }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-
-                }
-//                for(int i = 0; i < nums.length; i++) {
-//                    System.out.println(intNum[i]);
-//                }
-            }
-
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
+    public double distance(Point other) {
+        return Math.sqrt((Math.pow((x - other.x),2)) + (Math.pow((y - other.y),2)));
     }
 }
--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,13 +1,39 @@
-import java.lang.reflect.Array;
-
 public class Rectangle {
-    public boolean isValid(Point M[][]){
-        if()
+    Point p1;
+    Point p2;
+    Point p3;
+    Point p4;
+    double s1;
+    double s2;
 
+
+    public Rectangle(Point p1, Point p2, Point p3, Point p4) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+        s1 = p1.distance(p2);
+        s2 = p1.distance(p4);
 
     }
-    public double area(int side){
-return 2.5;
+
+    public boolean isValid(){
+        if((s1 == (p4.distance(p3))) && (s2 == (p2.distance(p3)))){
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    public boolean isSquare(){
+        if((s1 == s2) && (isValid())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double area(){
+        return (s1 * s2);
+    }
 }
